e19928fb944ecb213fc1a2f41bf6f5a05e14102a
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-12-04 14:24:17 -0800

    Update projects schema.

--- models/project.js
+++ models/project.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const projectsSchema = new Schema ({
-    title: String
+    title: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    timestamps: true
+
 })
 
 module.exports = mongoose.model('Movie', projectsSchema);
