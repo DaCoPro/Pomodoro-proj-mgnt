b3b6b943bf08ba46b7de1b058a593e01be4a5a2e
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-12-08 12:21:05 -0800

    Add timer display to tab. Add 0:09 when seconds go below 10.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,40 +4,45 @@ document.getElementById("workbtn")
     .addEventListener("click", pomodoro);
 document.getElementById("breakbtn")
     .addEventListener("click", pomodoro);
-document.getElementById("projList")
-    .addEventListener("click", projClick);
 
 //-------------------------------------------------CACHE
 
-let clickStatus = false;
 let timeDisplay = document.getElementById("timedisplay");
+let titleTimeDisp = document.getElementById("titletimedisp");
 
 //-------------------------------------------------FUNCTIONS
+
+//page default
+let clickStatus = false;
 
 function pomodoro(evt) {
     if (clickStatus) return;
     let interval;
     let seconds = 0;
     if(evt.target.id === 'workbtn') {
-        seconds = 25 * 60;
+        seconds = 5;
     } else if (evt.target.id === 'breakbtn') {
-        seconds = 5 * 60;
+        seconds = 5;
     }
     clickStatus = true;
     interval = setInterval(function() {
         let minutes = Math.floor(seconds / 60);
         let secs = Math.floor(seconds % 60);
-        timeDisplay.innerHTML = `${minutes} : ${secs}`;
+        if (secs < 10) {
+            timeDisplay.innerHTML = `${minutes} : 0${secs}`;
+            titleTimeDisp.innerHTML = `${minutes} : 0${secs}`;
+        } else {
+           timeDisplay.innerHTML = `${minutes} : ${secs}`; 
+           titleTimeDisp.innerHTML = `${minutes} : ${secs}`; 
+        }
+        
         seconds--;
         if(!seconds) {
             clearInterval(interval);
             clickStatus = false;
+            titleTimeDisp.innerHTML = 'Ding!';
+            timeDisplay.innerHTML = "--:--";
+            
         }
     }, 1000)
 }
-
-function projClick(evt) {
-    console.log(projects);
-}
-
-
