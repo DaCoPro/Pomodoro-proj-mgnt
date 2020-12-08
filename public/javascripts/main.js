5b6d0455729832b44fb011e6ebf07cfc09bd0a5c
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-12-07 16:45:46 -0800

    Add responsive timer display.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,32 @@
-document.getElementById("startbtn")
-    .addEventListener('click', function() {
-        let interval;
-        let seconds = 50; 
-        interval = setInterval(function() {
-            console.log(seconds);
-            seconds--;
-            if(!seconds) {
-                clearInterval(interval);
-                alert("Time's up!");
-            }
-        }, 1000)
-});
+document.getElementById("workbtn")
+    .addEventListener('click', clickBtn);
+document.getElementById("breakbtn")
+    .addEventListener('click', clickBtn);
+
+let clickStatus = false;
+let timeDisplay = document.getElementById("timedisplay");
+
+function clickBtn(evt) {
+    if (clickStatus) return;
+    let interval;
+    let seconds = 0;
+    if(evt.target.id === 'workbtn') {
+        seconds = 25 * 60;
+    } else if (evt.target.id === 'breakbtn') {
+        seconds = 5 * 60;
+    }
+    clickStatus = true;
+    interval = setInterval(function() {
+        let minutes = Math.floor(seconds / 60);
+        let secs = Math.floor(seconds % 60);
+        timeDisplay.innerHTML = `${minutes} : ${secs}`;
+        seconds--;
+        if(!seconds) {
+            clearInterval(interval);
+            clickStatus = false;
+        }
+    }, 1000)
+}
+
+
 
