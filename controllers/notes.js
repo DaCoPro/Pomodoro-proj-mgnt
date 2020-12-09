657485a5e371de2af823a259cba285b3c611d993
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-12-08 22:49:21 -0800

    Add delete function for notes.

--- controllers/notes.js
+++ controllers/notes.js
@@ -2,6 +2,7 @@ const Project = require('../models/project');
 
 module.exports = {
     create,
+    delete: deleteNote,
 };
 
 function create (req, res) {
@@ -14,4 +15,17 @@ function create (req, res) {
             res.redirect(`/projects/${project._id}`);
         });
     });
+}
+
+function deleteNote(req, res, next) {  
+    Project.findOne({'notes._id':req.params.id})
+        .then(function(project) {
+            const note = project.notes.id(req.params.id);
+            note.remove();
+            project.save().then(function() {
+                res.redirect(`/projects/${project._id}`);
+            }).catch(function(err) {
+                return next(err);
+            });
+        });
 }
--- routes/notes.js
+++ routes/notes.js
@@ -4,5 +4,6 @@ const notesCtrl = require('../controllers/notes');
 const isLoggedIn = require('../config/auth');
 
 router.post('/projects/:id/notes', isLoggedIn, notesCtrl.create);
+router.delete('/notes/:id', isLoggedIn, notesCtrl.delete);
 
 module.exports = router;
